80ce1788b35e808a58f2f605ab8365535d12b522
Leonardo Eugênio <lelgenio@disroot.org>
2022-06-10 00:22:53 -0300

    up

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My system config";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        i15 = lib.nixosSystem {
+          inherit system;
+          modules = [ ./system/configuration.nix ];
+        };
+      };
+
+    };
+}
