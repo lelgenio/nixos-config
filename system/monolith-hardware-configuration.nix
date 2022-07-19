340dfa5fc0cef133077fb4058d80bbe1ccd7756c
Leonardo Eugênio <lelgenio@disroot.org>
2022-07-18 23:32:53 -0300

    update monolith config

--- system/monolith-hardware-configuration.nix
+++ system/monolith-hardware-configuration.nix
@@ -2,33 +2,45 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
+
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
+
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/bea0fbbd-c1a2-4379-8cc5-658507e36032";
+    { device = "/dev/disk/by-uuid/876ccabe-511b-4cd4-9d4e-5da899c940ca";
       fsType = "btrfs";
-      options = [ "subvol=@" ];
+      options = [ "subvol=nixos" ];
     };
-  boot.initrd.luks.devices."luks-fb41335e-cca7-47e2-a886-24549b8accb8".device = "/dev/disk/by-uuid/fb41335e-cca7-47e2-a886-24549b8accb8";
+
+  boot.initrd.luks.devices."main".device = "/dev/disk/by-uuid/325ca6b7-0bcd-469d-bfb1-ba5667342871";
+
   fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/E2D7-919A";
+    { device = "/dev/disk/by-uuid/FF2D-8819";
       fsType = "vfat";
     };
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/3e5781c7-5e56-4b5d-9720-12b384fad726"; }
-    ];
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/876ccabe-511b-4cd4-9d4e-5da899c940ca";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
+
+  swapDevices = [ ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   networking.hostName = "monolith"; # Define your hostname.
 }
