6d75e4b4aa681ba7b9faeb4954b8d3d53653156a
Leonardo Eugênio <lelgenio@disroot.org>
2022-07-20 22:19:07 -0300

    remove user switch option

--- system-switch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-HERE="$(dirname "$0")"
-# sudo nixos-rebuild switch -I nixos-config="$HERE/system/configuration.nix"
-sudo nixos-rebuild switch --verbose --flake .#
--- user-switch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-HERE="$(dirname "$0")"
-home-manager switch -f "$HERE/user/home.nix"
