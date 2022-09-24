ba18c4944b2a655689d4b55f6c662b8ee40588cb
Leonardo Eugênio <lelgenio@disroot.org>
2022-09-23 21:25:05 -0300

    sway: fix systemd variables

--- user/sway.nix
+++ user/sway.nix
@@ -38,6 +38,16 @@ let
         systemctl --user start swayidle.service
     fi
   '';
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
 in {
   config = {
     wayland.windowManager.sway = let
@@ -311,6 +321,7 @@ in {
       };
       extraConfig = ''
         for_window [title=.*] inhibit_idle fullscreen
+        exec ${dbus-sway-environment}/bin/dbus-sway-environment
       '';
     };
     services.swayidle = {
