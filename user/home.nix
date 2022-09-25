09bb3e5a7b9e4e0ead73dddb3697fb06cfe19bdf
Leonardo Eugênio <lelgenio@disroot.org>
2022-09-25 00:12:58 -0300

    add mpv

--- user/home.nix
+++ user/home.nix
@@ -279,4 +279,18 @@ in {
       Install = { WantedBy = [ "sway-session.target" ]; };
     };
   };
+  programs.mpv.enable = true;
+  programs.mpv.config = {
+    # ytdl-format='best';
+    ytdl_path="yt-dlp";
+    ytdl-format="bestvideo[height<=1080][vcodec!=vp9]+bestaudio/best";
+    ytdl-raw-options="cookies=~/.cache/cookies-youtube-com.txt,mark-watched=";
+    osd-fractions=true;
+    save-position-on-quit=true;
+    keep-open=true;
+
+    cache=true;
+    cache-pause-initial=true;
+    cache-pause-wait=10;
+  };
 }
