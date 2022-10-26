95efe2cd3770331cb3c9326617aa9174dcf615a5
lelgenio <lelgenio@disroot.org>
2022-10-26 15:01:55 -0300

    add swap to rainbow

--- hosts/rainbow.nix
+++ hosts/rainbow.nix
@@ -38,7 +38,10 @@
       options = [ "subvol=@swap" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/swap/swapfile";
+    size = (1024 * 4); # RAM size + 2 GB
+  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
