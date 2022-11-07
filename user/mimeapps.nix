838a2b3b65f96a1400ed6b1bb04436fd637f3a48
Leonardo Eugênio <lelgenio@disroot.org>
2022-11-07 12:54:09 -0300

    add desktop entries

--- user/mimeapps.nix
+++ user/mimeapps.nix
@@ -2,6 +2,53 @@
 let inherit (import ./variables.nix) key theme color accent font;
 in {
   config = {
+    xdg.desktopEntries = {
+      kak = {
+        name = "Kakoune";
+        genericName = "Text Editor";
+        comment = "Edit text files";
+        exec = "kak %F";
+        terminal = true;
+        type = "Application";
+        icon = "kak.desktop";
+        categories = [ "Utility" "TextEditor" ];
+        startupNotify = true;
+        mimeType = [
+          "text/english"
+          "text/plain"
+          "text/x-makefile"
+          "text/x-c++hdr"
+          "text/x-c++src"
+          "text/x-chdr"
+          "text/x-csrc"
+          "text/x-java"
+          "text/x-moc"
+          "text/x-pascal"
+          "text/x-tcl"
+          "text/x-tex"
+          "application/x-shellscript"
+          "text/x-c"
+          "text/x-c++"
+        ];
+        settings = {
+          Keywords = "Text;editor;";
+          TryExec = "kak";
+        };
+      };
+      neomutt = {
+        name = "Neomutt";
+        genericName = "Email Client";
+        comment = "View and Send Emails";
+        exec = "terminal neomutt %U";
+        terminal = false;
+        type = "Application";
+        icon = "mutt";
+        categories = [ "Network" "Email" ];
+        startupNotify = false;
+        mimeType = [ "x-scheme-handler/mailto" ];
+        settings = { Keywords = "Mail;E-mail;"; };
+      };
+    };
     xdg.mimeApps = let
 
       createMimeAssociation = (mime_prefix: application: mime_suffix: {
