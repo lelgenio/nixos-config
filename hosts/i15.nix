5486bbd5de170146abdac9e8f94dd197736f57b2
Leonardo Eugênio <lelgenio@disroot.org>
2022-12-19 23:09:36 -0300

    i15: update partitions

--- hosts/i15.nix
+++ hosts/i15.nix
@@ -12,18 +12,27 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-label/NIX_BOOT";
+    fsType = "vfat";
+  };
+
   fileSystems."/" = {
-    device = "/dev/disk/by-label/ROOT";
+    device = "/dev/disk/by-label/NIX_ROOT";
     fsType = "btrfs";
     options = [ "subvol=nixos" "compress=zstd" "noatime" ];
   };
 
-  fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-label/NIXBOOT";
-    fsType = "vfat";
+  fileSystems."/home" = {
+    device = "/dev/disk/by-label/NIX_ROOT";
+    fsType = "btrfs";
+    options = [ "subvol=home" "compress=zstd" "noatime" ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/swap/swapfile";
+    size = (1024 * 8) + (1024 * 2); # RAM size + 2 GB
+  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
