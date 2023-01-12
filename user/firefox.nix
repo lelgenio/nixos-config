d73090a19291b7b2ad7c298491e9ea87b60a8000
Leonardo Eugênio <lelgenio@disroot.org>
2023-01-11 21:00:30 -0300

    firefox: disable media RDD

--- user/firefox.nix
+++ user/firefox.nix
@@ -20,9 +20,22 @@ in {
             "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
             "browser.tabs.inTitlebar" = if desktop == "sway" then 0 else 1;
 
-            "media.ffmpeg.vaapi.enabled" = true;
-            "media.ffvpx.enabled" = true;
+            # disable media RDD because it's buggy and crashes the gpu
+            "media.rdd-ffmpeg.enabled" = false;
+            "media.rdd-ffvpx.enabled" = false;
+            "media.rdd-opus.enabled" = false;
+            "media.rdd-process.enabled" = false;
+            "media.rdd-retryonfailure.enabled" = false;
+            "media.rdd-theora.enabled" = false;
+            "media.rdd-vorbis.enabled" = false;
+            "media.rdd-vpx.enabled" = false;
+            "media.rdd-wav.enabled" = false;
+
             "media.av1.enabled" = true;
+            "media.ffmpeg.vaapi-drm-display.enabled" = true;
+            "media.ffmpeg.vaapi.enabled" = true;
+            "media.ffvpx.enabled" = false;
+
             "gfx.webrender.all" = true;
           };
           userChrome = lib.mkIf (desktop == "sway") ''
@@ -46,6 +59,9 @@ in {
         Install = { WantedBy = [ "sway-session.target" ]; };
       };
     };
-    home.sessionVariables = { MOZ_ENABLE_WAYLAND = "1"; };
+    home.sessionVariables = {
+      MOZ_ENABLE_WAYLAND = "1";
+      MOZ_DISABLE_RDD_SANDBOX = "1";
+    };
   };
 }
