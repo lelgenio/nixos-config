bd31b35e6b4ebbff6267772818e314e90c61ac04
Leonardo Eugênio <lelgenio@disroot.org>
2023-02-08 12:43:50 -0300

    pipewire: don't restart service on sway init

--- overlays/sway.nix
+++ overlays/sway.nix
@@ -12,8 +12,8 @@
     text = ''
       systemctl --user import-environment
       dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
+      # systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
+      # systemctl --user start pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
     '';
   };
 })
