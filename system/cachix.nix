abfddc0655f3d06cd2e485c3540caaeb7c24b02e
Leonardo Eugênio <lelgenio@disroot.org>
2023-02-08 12:42:45 -0300

    cachix: fix shutdown hang

--- system/cachix.nix
+++ system/cachix.nix
@@ -1,8 +1,12 @@
-{ pkgs, config, ... }: {
+{ pkgs, lib, config, ... }: {
   services.cachix-watch-store = {
     enable = true;
     cacheName = "lelgenio";
     cachixTokenFile = config.age.secrets.lelgenio-cachix.path;
   };
-  systemd.services.cachix-watch-store-agent.serviceConfig.TimeoutStopSec = 3;
+  systemd.services.cachix-watch-store-agent = {
+    serviceConfig.TimeoutStopSec = 3;
+    # If we don't do this, cachix tends to timeout
+    serviceConfig.KillMode = lib.mkForce "control-group";
+  };
 }
