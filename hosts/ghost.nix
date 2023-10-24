3ebd680633a003f90f4705e8a042ffdc25483f3d
Leonardo Eugênio <lelgenio@disroot.org>
2023-10-24 10:57:44 -0300

    ghost: enable syncthing

--- hosts/ghost.nix
+++ hosts/ghost.nix
@@ -94,6 +94,32 @@
     defaults.email = "lelgenio@disroot.org";
   };
 
+  services.syncthing = {
+    enable = true;
+    dataDir = "/var/lib/syncthing-data";
+    guiAddress = "0.0.0.0:8384";
+    openDefaultPorts = true;
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+  };
+  services.nginx.virtualHosts."syncthing.lelgenio.xyz" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:8384";
+      extraConfig =
+        # required when the target is also TLS server with multiple hosts
+        "proxy_ssl_server_name on;" +
+        # required when the server wants to use HTTP Authentication
+        "proxy_pass_header Authorization;"
+      ;
+    };
+  };
+
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
   swapDevices = [{
