a18f2791c237446a8718fed64d4cb10bbc612605
Leonardo Eugênio <lelgenio@disroot.org>
2023-11-07 20:46:08 -0300

    mpd: don't enable mpd for gnome

--- user/mpd.nix
+++ user/mpd.nix
@@ -1,27 +1,29 @@
-{ pkgs, config, ... }: {
-  services.mpd = {
-    enable = true;
-    musicDirectory = config.home.homeDirectory + "/Música";
-    extraConfig = ''
-      restore_paused "yes"
-      auto_update "yes"
-      audio_output {
-          type    "pulse"
-          name    "My Pulse Output"
-          mixer_type  "hardware"
-      }
-      filesystem_charset    "UTF-8"
-    '';
+{ lib, pkgs, config, ... }: {
+  config = lib.mkIf (pkgs.uservars.desktop != "gnome") {
+    services.mpd = {
+      enable = true;
+      musicDirectory = config.home.homeDirectory + "/Música";
+      extraConfig = ''
+        restore_paused "yes"
+        auto_update "yes"
+        audio_output {
+            type    "pulse"
+            name    "My Pulse Output"
+            mixer_type  "hardware"
+        }
+        filesystem_charset    "UTF-8"
+      '';
+    };
+    services.mpdris2 = {
+      enable = true;
+      multimediaKeys = true;
+      notifications = true;
+    };
+    home.packages = with pkgs; [
+      musmenu
+      python3Packages.deemix
+      dzadd
+      mpdDup
+    ];
   };
-  services.mpdris2 = {
-    enable = true;
-    multimediaKeys = true;
-    notifications = true;
-  };
-  home.packages = with pkgs; [
-    musmenu
-    python3Packages.deemix
-    dzadd
-    mpdDup
-  ];
 }
