3094240717c91b875e1681be2239dab2e42dfaed
Leonardo Eugênio <lelgenio@disroot.org>
2023-11-11 13:20:39 -0300

    power: suspend on power button

--- system/configuration.nix
+++ system/configuration.nix
@@ -144,6 +144,10 @@
   };
   security.sudo.wheelNeedsPassword = false;
 
+  services.logind.extraConfig = ''
+    HandlePowerKey=suspend
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
