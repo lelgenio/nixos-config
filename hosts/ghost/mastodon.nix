3830a01240cd75bf42d84e367fefc3988203d023
Leonardo Eugênio <lelgenio@disroot.org>
2024-02-02 20:53:36 -0300

    ghost: update mastodon to fix a vulnerability

--- hosts/ghost/mastodon.nix
+++ hosts/ghost/mastodon.nix
@@ -6,6 +6,15 @@
     smtp.fromAddress = "lelgenio@disroot.org";
     extraConfig.SINGLE_USER_MODE = "true";
     streamingProcesses = 2;
+    package = pkgs.mastodon.override {
+      version = "4.2.5";
+      patches = [
+        (pkgs.fetchpatch {
+          url = "https://github.com/mastodon/mastodon/compare/v4.2.4...v4.2.5.patch";
+          hash = "sha256-CtzYV1i34s33lV/1jeNcr9p/x4Es1zRaf4l1sNWVKYk=";
+        })
+      ];
+    };
   };
 
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
