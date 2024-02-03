35e0704b82951d2b0b43f3d406a39c86adc141ac
Leonardo Eugênio <lelgenio@disroot.org>
2024-02-02 21:03:34 -0300

    agenix: fix secrets not being opened on boot
    Agenix runs before some partitions are mounted,
    one option is to mark those partitions are need for boot:
    
    {
      fileSystems."/home".neededForBoot = true;
    }
    
    I choose to copy my ssh key to the root user's HOME,
    which is not in a separate partition.

--- system/secrets.nix
+++ system/secrets.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   age = {
-    identityPaths = [ "/home/lelgenio/.ssh/id_rsa" ];
+    identityPaths = [ "/root/.ssh/id_rsa" ];
     secrets.lelgenio-cachix.file = ../secrets/lelgenio-cachix.age;
     secrets.monolith-gitlab-runner-thoreb-itinerario-registrationConfigFile.file =
       ../secrets/monolith-gitlab-runner-thoreb-itinerario-registrationConfigFile.age;
