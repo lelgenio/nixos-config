325cf58e2ce9d3461cf546c4f5227036a787aa76
Leonardo Eugênio <lelgenio@disroot.org>
2024-02-07 12:45:30 -0300

    thunar: enable krita thumbnails

--- system/thunar.nix
+++ system/thunar.nix
@@ -10,4 +10,19 @@
   services.gvfs.enable = true;
   # Thumbnail support for images
   services.tumbler.enable = true;
+
+  environment.systemPackages = [
+    (
+      pkgs.writeTextFile {
+        name = "thumbs";
+        text = ''
+          [Thumbnailer Entry]
+          TryExec=unzip
+          Exec=sh -c "${pkgs.unzip}/bin/unzip -p %i preview.png > %o"
+          MimeType=application/x-krita;
+        '';
+        destination = "/share/thumbnailers/kra.thumbnailer";
+      }
+    )
+  ];
 }
