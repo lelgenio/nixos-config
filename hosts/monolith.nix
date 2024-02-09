b0bd97336543107496d13051586c69b3025463a5
Leonardo Eugênio <lelgenio@disroot.org>
2024-02-09 19:12:31 -0300

    monolith: add projects mount

--- hosts/monolith.nix
+++ hosts/monolith.nix
@@ -101,6 +101,11 @@ in
     fsType = "btrfs";
     options = [ "nofail" ] ++ btrfs_options ++ btrfs_ssd;
   };
+  fileSystems."/home/lelgenio/projects" = {
+    device = "/dev/disk/by-label/BTRFS_BIGBOY";
+    fsType = "btrfs";
+    options = [ "subvol=@projects" "nofail" ] ++ btrfs_options ++ btrfs_ssd;
+  };
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
