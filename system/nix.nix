e7d0121d466cb84f241e68fe04ef9f227736e394
Leonardo Eugênio <lelgenio@disroot.org>
2024-02-19 12:05:14 -0300

    nix: add nix-community substituter

--- system/nix.nix
+++ system/nix.nix
@@ -11,6 +11,8 @@
     settings = {
       auto-optimise-store = true;
       substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
         # "http://nixcache.lelgenio.1337.cx:5000"
         "https://hyprland.cachix.org"
         "https://lelgenio.cachix.org"
@@ -18,6 +20,8 @@
         "https://snowflakeos.cachix.org/"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         # "nixcache.lelgenio.1337.cx:zxCfx7S658llDgAUG0JVyNrlAdFVvPniSdDOkvfTPS8="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "lelgenio.cachix.org-1:W8tMlmDFLU/V+6DlChXjekxoHZpjgVHZpmusC4cueBc="
