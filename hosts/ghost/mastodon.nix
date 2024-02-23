a0531650c6725afb5986cc25d2ab253b8d67a5d8
Leonardo Eugênio <lelgenio@disroot.org>
2024-02-23 12:01:09 -0300

    mastodon: remove unnecessary configurations

--- hosts/ghost/mastodon.nix
+++ hosts/ghost/mastodon.nix
@@ -1,16 +1,10 @@
 { config, pkgs, inputs, ... }: {
   services.mastodon = {
     enable = true;
-    localDomain = "social.lelgenio.xyz";
     configureNginx = true;
+    localDomain = "social.lelgenio.xyz";
     smtp.fromAddress = "lelgenio@disroot.org";
-    extraConfig.SINGLE_USER_MODE = "true";
     streamingProcesses = 2;
-  };
-
-  services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
-    forceSSL = true;
-    enableACME = true;
+    extraConfig.SINGLE_USER_MODE = "true";
   };
 }
-
