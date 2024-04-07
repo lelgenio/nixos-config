9165fd4b6f40f8d35ae1c3127d97cef12171edc8
Leonardo Eugênio <lelgenio@disroot.org>
2024-04-06 23:41:34 -0300

    forgejo: remove unnecessary ssh configuration

--- hosts/phantom/forgejo.nix
+++ hosts/phantom/forgejo.nix
@@ -15,14 +15,6 @@ in
     };
   };
 
-  services.openssh = {
-    authorizedKeysFiles = [
-      "${config.services.forgejo.stateDir}/.ssh/authorized_keys"
-    ];
-    # Recommended by forgejo: https://forgejo.org/docs/latest/admin/recommendations/#git-over-ssh
-    settings.AcceptEnv = "GIT_PROTOCOL";
-  };
-
   services.forgejo = {
     enable = true;
     database.type = "postgres";
