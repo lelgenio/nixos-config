418a142f15f7650929a1b3bffdaf348e3ba2f470
Leonardo Eugênio <lelgenio@disroot.org>
2024-05-11 18:32:05 -0300

    mastodon: configure noreply email

--- hosts/phantom/mastodon.nix
+++ hosts/phantom/mastodon.nix
@@ -3,9 +3,21 @@
     enable = true;
     configureNginx = true;
     localDomain = "social.lelgenio.com";
-    smtp.fromAddress = "lelgenio@disroot.org";
+    smtp = {
+      authenticate = true;
+      host = "lelgenio.com";
+      fromAddress = "noreply@social.lelgenio.com";
+      user = "noreply@social.lelgenio.com";
+      passwordFile = config.age.secrets.phantom-mastodon-mailer-password.path;
+    };
     streamingProcesses = 2;
     extraConfig.SINGLE_USER_MODE = "true";
     mediaAutoRemove.olderThanDays = 10;
+  };
+
+  age.secrets.phantom-mastodon-mailer-password = {
+    file = ../../secrets/phantom-mastodon-mailer-password.age;
+    mode = "400";
+    owner = "mastodon";
   };
 }
