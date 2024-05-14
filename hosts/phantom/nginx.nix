c40cbf74f38ffec18a0541f3fc2368cf95a841b8
Leonardo Eugênio <lelgenio@disroot.org>
2024-05-14 16:56:09 -0300

    nginx: add .xyz -> .com redirect

--- hosts/phantom/nginx.nix
+++ hosts/phantom/nginx.nix
@@ -1,9 +1,22 @@
-{ config, pkgs, inputs, ... }: {
+{ config, pkgs, lib, ... }: {
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
   };
+
+  # Redirect *lelgenio.xyz -> *lelgenio.com
+  services.nginx.virtualHosts = lib.mapAttrs'
+    (key: value: lib.nameValuePair "${key}lelgenio.xyz" value)
+    (
+      lib.genAttrs [ "" "social." "blog." "cloud." "mail." ] (name: {
+        enableACME = true;
+        forceSSL = true;
+        locations."/".return = "301 $scheme://${name}lelgenio.com$request_uri";
+      })
+    );
 
   security.acme = {
     acceptTerms = true;
