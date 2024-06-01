dca5bba67e78248a0c405b05309a72bf0229eedd
Leonardo Eugênio <lelgenio@disroot.org>
2024-05-31 23:10:41 -0300

    gnome: enable variable refresh rate

--- system/gnome.nix
+++ system/gnome.nix
@@ -1,13 +1,18 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    enable = true;
+    desktopManager.gnome = {
+      enable = true;
+      # Enable VRR (Variable Refresh Rate)
+      extraGSettingsOverridePackages = with pkgs; [ gnome.mutter ];
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        experimental-features=['variable-refresh-rate', 'scale-monitor-framebuffer']
+      '';
+    };
+    displayManager.gdm.enable = true;
+  };
 
   # Workaround for https://github.com/NixOS/nixpkgs/issues/103746
   systemd.services."getty@tty1".enable = false;
