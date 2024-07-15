4a61403ede58c6f60377ab93aaee71e08d10536c
Leonardo Eugênio <lelgenio@disroot.org>
2024-07-15 12:50:49 -0300

    invidious: fix conflic with forgejo port

--- hosts/phantom/invidious.nix
+++ hosts/phantom/invidious.nix
@@ -1,20 +1,12 @@
-{ config, ... }:
 {
   services.invidious = {
     enable = true;
     domain = "invidious.lelgenio.com";
     nginx.enable = true;
+    port = 10601;
     settings.db = {
       user = "invidious";
       dbname = "invidious";
-    };
-  };
-
-  services.nginx = {
-    clientMaxBodySize = "100m";
-    virtualHosts.${config.services.invidious.domain} = {
-      enableACME = true;
-      forceSSL = true;
     };
   };
 }
