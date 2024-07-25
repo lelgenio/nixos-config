21fc3e6d719e61d3cb324876c14253ddabeaad95
Leonardo Eugênio <lelgenio@disroot.org>
2024-07-24 21:18:01 -0300

    keyd: fix modifier keys (rightshift bacame leftshift)

--- system/keyboard.nix
+++ system/keyboard.nix
@@ -8,6 +8,16 @@
         "main" = {
           # Some programs don't respect colemaks capslock bind, so we force it here
           "capslock" = "backspace";
+
+          # keyd has incorrect defaults :/
+          "leftshift" = "leftshift";
+          "rightshift" = "rightshift";
+          "leftalt" = "leftalt";
+          "rightalt" = "rightalt";
+          "leftcontrol" = "leftcontrol";
+          "rightcontrol" = "rightcontrol";
+          "leftmeta" = "leftmeta";
+          "rightmeta" = "rightmeta";
         };
       };
     };
