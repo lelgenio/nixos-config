914d2ffde574e94bb5da161d8d0057be3adff4e7
Leonardo Eugênio <lelgenio@disroot.org>
2024-08-20 10:50:40 -0300

    davi: extract configuration

--- hosts/phantom/users.nix
+++ hosts/phantom/users.nix
@@ -41,22 +41,6 @@
     initialHashedPassword = "$y$j9T$E3aBBSSq0Gma8hZD9L7ov0$iCGDW4fqrXWfHO0qodBYYgMFA9CpIraoklHcPbJJrM3";
   };
 
-  users.users.davikiwi = {
-    isNormalUser = true;
-    description = "Davi";
-    hashedPassword = "$y$j9T$0e/rczjOVCy7PuwC3pG0V/$gTHZhfO4wQSlFvbDyfghbCnGI2uDI0a52zSrQ/yOA5A";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGgZDBnj+gVMHqoNvjpx2T/HqnxUDbLPshu+t7301gXd Davi@DESKTOP-EVHFGJ9"
-    ];
-    extraGroups = [ "docker" ];
-    packages = with pkgs; [
-      (pkgs.python3.withPackages (python-pkgs: [
-        python-pkgs.pip
-        python-pkgs.wheel
-      ]))
-    ];
-  };
-
   security.sudo.wheelNeedsPassword = false;
 
   programs.fish.enable = true;
