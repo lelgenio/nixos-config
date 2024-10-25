5bda7a8481d577210a096f48944577168e2c5f12
Leonardo Eugênio <lelgenio@disroot.org>
2024-10-25 16:43:53 -0300

    ssh: include private configs

--- user/ssh.nix
+++ user/ssh.nix
@@ -1,26 +1,20 @@
 {
-  config,
-  pkgs,
-  lib,
-  ...
-}:
-let
-  inherit (config.my) username mail;
-in
-{
   config = {
-    programs.ssh.enable = true;
-    programs.ssh.matchBlocks = {
-      monolith = {
-        user = "lelgenio";
-        hostname = "monolith.lelgenio.com";
-        port = 9022;
+    programs.ssh = {
+      enable = true;
+      matchBlocks = {
+        monolith = {
+          user = "lelgenio";
+          hostname = "monolith.lelgenio.com";
+          port = 9022;
+        };
+        phantom = {
+          user = "root";
+          hostname = "phantom.lelgenio.com";
+          port = 9022;
+        };
       };
-      phantom = {
-        user = "root";
-        hostname = "phantom.lelgenio.com";
-        port = 9022;
-      };
+      includes = [ "~/Wopus/.ssh.config" ];
     };
   };
 }
