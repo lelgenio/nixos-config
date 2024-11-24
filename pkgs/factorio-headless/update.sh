8dfbe66e1548d4ad0b9b03a5c9e2b9180afd083b
Leonardo Eugênio <lelgenio@disroot.org>
2024-11-23 23:41:28 -0300

    factorio: 2.0.15 -> 2.0.20

--- pkgs/factorio-headless/update.sh
+++ pkgs/factorio-headless/update.sh
@@ -10,5 +10,5 @@ current_hash="$(rg '^.*?hash\s*=\s*"(.+)".*?$' --replace '$1' ./default.nix)"
 new_version="$(curl https://factorio.com/api/latest-releases | jq -r .stable.headless)"
 new_hash="$(nix-hash --to-sri --type sha256 $(nix-prefetch-url --type sha256 https://www.factorio.com/get-download/${new_version}/headless/linux64))"
 
-sd "$current_version" "$new_version" ./default.nix
-sd "$current_hash" "$new_hash" ./default.nix
+sd --fixed-strings "$current_version" "$new_version" ./default.nix
+sd --fixed-strings "$current_hash" "$new_hash" ./default.nix
