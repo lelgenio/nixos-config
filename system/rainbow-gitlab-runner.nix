77adcf8951b49d8a24e4501f1429483d95708f10
lelgenio <lelgenio@lelgenio.com>
2025-01-23 16:07:09 -0300

    rainbow: update gitlab concurrent runners

--- system/rainbow-gitlab-runner.nix
+++ system/rainbow-gitlab-runner.nix
@@ -12,7 +12,7 @@ in
   virtualisation.docker.enable = true;
   services.gitlab-runner = {
     enable = true;
-    settings.concurrent = 1;
+    settings.concurrent = 6;
     services = {
       thoreb-telemetria-nix = mkNixRunner config.age.secrets.gitlab-runner-thoreb-telemetria-registrationConfigFile.path;
       thoreb-itinerario-nix = mkNixRunner config.age.secrets.rainbow-gitlab-runner-thoreb-itinerario-registrationConfigFile.path;
