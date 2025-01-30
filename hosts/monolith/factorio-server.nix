43b501b261da43e7d506800b28b66989cc41815a
Leonardo Eugênio <lelgenio@disroot.org>
2025-01-29 22:46:04 -0300

    factorio: auto backup the game saves

--- hosts/monolith/factorio-server.nix
+++ hosts/monolith/factorio-server.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   services.factorio = {
     enable = true;
@@ -13,6 +18,28 @@
   systemd.services.factorio = {
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
+  };
+
+  systemd.services.factorio-backup-save = {
+    description = "Backup factorio saves";
+    script = ''
+      ${lib.getExe pkgs.rsync} \
+        -av  \
+        --chown=lelgenio \
+        /var/lib/factorio/saves/default.zip \
+        ~lelgenio/Documentos/GameSaves/factorio_saves/space-age-$(date --iso=seconds).zip
+    '';
+    serviceConfig.Type = "oneshot";
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.timers.factorio-backup-save = {
+    timerConfig = {
+      OnCalendar = "*-*-* 18:00:00";
+      Persistent = true;
+      Unit = "factorio-backup-save.service";
+    };
+    wantedBy = [ "timers.target" ];
   };
 
   age.secrets.factorio-settings = {
