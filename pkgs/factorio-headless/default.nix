cef96416d86f962ad52aa996a15815ef455a6ef9
Leonardo Eugênio <lelgenio@disroot.org>
2025-03-12 23:29:31 -0300

    factorio: 2.0.32 -> 2.0.39

--- pkgs/factorio-headless/default.nix
+++ pkgs/factorio-headless/default.nix
@@ -1,10 +1,10 @@
 { factorio-headless, pkgs }:
 
 factorio-headless.overrideAttrs (_: rec {
-  version = "2.0.32";
+  version = "2.0.39";
   src = pkgs.fetchurl {
     name = "factorio_headless_x64-${version}.tar.xz";
     url = "https://www.factorio.com/get-download/${version}/headless/linux64";
-    hash = "sha256-KmECrkLcxej+kjvWi80yalaeNZEqzeEhMB5dTS2FZBc=";
+    hash = "sha256-D4o9DkN5e1/02LhdfDNLCVo/B9mqf4Cx6H+Uk5qT3zQ=";
   };
 })
