310f3b192c157bbac4595a25a7c8e844a20543fc
Leonardo Eugênio <lelgenio@disroot.org>
2025-04-08 21:42:08 -0300

    qt: cleanup theme config

--- user/sway/theme.nix
+++ user/sway/theme.nix
@@ -55,7 +55,7 @@ lib.mkIf (desktop == "sway") {
   qt = {
     enable = true;
     platformTheme.name = "gtk3";
-    style.name = "qt5ct";
+    style.name = "kvantum";
   };
 
   dconf.settings = {
@@ -70,28 +70,6 @@ lib.mkIf (desktop == "sway") {
     };
   };
 
-  # fonts.fontconfig.enable = true;
-  xdg.configFile = {
-    "qt5ct/qt5ct.conf".text = ''
-      [Appearance]
-      # color_scheme_path=/nix/store/f07mk0vrm47jxw3y5v99hxncy0w4vcyq-qt5ct-1.5/share/qt5ct/colors/darker.conf
-      custom_palette=false
-      icon_theme=${icon_theme}
-      standard_dialogs=default
-      style=kvantum-dark
-
-      # [Fonts]
-      # fixed=@Variant(\0\0\0@\0\0\0\x1c\0H\0\x61\0\x63\0k\0 \0N\0\x65\0r\0\x64\0 \0\x46\0o\0n\0t@(\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
-      # general=@Variant(\0\0\0@\0\0\0\x1e\0L\0i\0\x62\0\x65\0r\0\x61\0t\0i\0o\0n\0 \0S\0\x61\0n\0s@(\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
-    '';
-    "kdedefaults/kdeglobals".text = ''
-      [General]
-      ColorScheme=BreezeDark
-
-      [Icons]
-      Theme=${icon_theme}
-    '';
-  };
   services.xsettingsd = {
     enable = true;
     settings = {
@@ -105,11 +83,6 @@ lib.mkIf (desktop == "sway") {
   };
 
   home.packages = with pkgs; [
-    libsForQt5.qt5ct
-    libsForQt5.qtstyleplugin-kvantum
-    qt6Packages.qt6ct
-    qt6Packages.qtstyleplugin-kvantum
-
     pkgs.bibata-cursors
     pkgs.orchis_theme_compact
     pkgs.papirus_red
